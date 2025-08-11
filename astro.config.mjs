ec2c267f6ac5664733b8eefeaf584e6e9d1bc354
hermes <hermeschigoziri@gmail.com>
2025-08-11 22:44:20 +0100

    add sanity cms to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sanity from "@sanity/astro";
+import { loadEnv } from "vite";
+
+// Load environment variables
+const env = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+
+	integrations: [
+		sanity({
+			projectId: env.PUBLIC_SANITY_PROJECT_ID,
+			dataset: env.PUBLIC_SANITY_DATASET,
+			apiVersion: "2025-08-11",
+		}),
+	],
 });
